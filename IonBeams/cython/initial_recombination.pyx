36e9d48b0aea312d99787f23f3e2c5f0bd4b8c8d
Jeppe Brage Christensen <jbrage@users.noreply.github.com>
2022-02-10 10:16:46 +0100

    Update initial_recombination.pyx

--- IonBeams/cython/initial_recombination.pyx
+++ IonBeams/cython/initial_recombination.pyx
@@ -18,7 +18,7 @@ def initial_PDEsolver(list parameter_list):
 
     # define the parameters from Kanai (1998)
     cdef double W = 33.9                # eV/ion pair for air
-    cdef double ion_mobility = 1.65     # cm s^-1 V^-1, averaged for positive and negative ions
+    cdef double ion_mobility = 1.65     # cm^2 s^-1 V^-1, averaged for positive and negative ions
     cdef double ion_diff = 3.7e-2       # cm^2/s, averaged for positive and negative ions
     cdef double alpha = 1.60e-6         # cm^3/s, recombination constant
 
