0cbe4ce94ccbfabe751c3dad1ca989b424859fcf
Jeppe Brage Christensen <jbrage@users.noreply.github.com>
2022-03-10 14:58:21 +0100

    Updated Geiss

--- IonBeams/single_track.py
+++ IonBeams/single_track.py
@@ -1,8 +1,9 @@
 import sys; sys.path.append('./cython')
 
+from Geiss_test import Geiss_PDEsolver
 from initial_recombination import initial_PDEsolver
 from functions import E_MeV_to_LET_keV_um, calc_b_cm, IC_angle_rad, Jaffe_theory
-
+import pyamtrack.libAT as libam
 
 
 def IonTracks_initial_recombination(voltage_V, energy_MeV, electrode_gap_cm,
@@ -13,6 +14,22 @@ def IonTracks_initial_recombination(voltage_V, energy_MeV, electrode_gap_cm,
     The function returns the inverse collection efficiency, i.e. the recombination
     correction factor k_s
     '''
+
+
+    beta = libam.AT_beta_from_E_single(energy_MeV)   
+    print("beta = {:0.3f}".format(beta))
+    
+    rho_material = 1.2e-3 # g/cm3
+    density_ratio = 1.0 / rho_material
+    a0_nm = 20 * beta
+    a0_nm = 1.5    
+    a0_cm = a0_nm * 1e-7  * density_ratio    
+    
+    unit_length_cm = 2.5e-4
+    
+    # unit_length_cm = a0_cm
+    
+    print(unit_length_cm / a0_cm)
 
     LET_keV_um = E_MeV_to_LET_keV_um(energy_MeV)
     track_radius_cm = calc_b_cm(LET_keV_um)
@@ -25,18 +42,22 @@ def IonTracks_initial_recombination(voltage_V, energy_MeV, electrode_gap_cm,
                                 IC_angle_rad,
                                 electrode_gap_cm,
                                 SHOW_PLOT,
-                                PRINT_parameters
+                                PRINT_parameters, 
+                                energy_MeV,
+                                a0_cm,
+                                unit_length_cm
                             ]
 
-    f_IonTracks = initial_PDEsolver(simulation_parameters)
+    # f_IonTracks = initial_PDEsolver(simulation_parameters)
+    f_IonTracks = Geiss_PDEsolver(simulation_parameters)    
     return 1./f_IonTracks
 
 
 if __name__ == "__main__":
 
-    electrode_gap_cm = 0.2
-    voltage_V = 50
-    energy_MeV = 226
+    electrode_gap_cm = 0.1
+    voltage_V = 200
+    energy_MeV = 20
 
     # LET in air
     LET_keV_um = E_MeV_to_LET_keV_um(energy_MeV)
