1a71a3fa09d37e7cba4075387ae9ef121f4e35d7
Christensen Jeppe Brage <christensen_j@pc13687.psi.ch>
2022-03-11 15:16:06 +0100

    test

--- IonBeams/single_track.py
+++ IonBeams/single_track.py
@@ -6,6 +6,27 @@ from functions import E_MeV_to_LET_keV_um, calc_b_cm, IC_angle_rad, Jaffe_theory
 import pyamtrack.libAT as libam
 
 
+
+
+s = """
+    stopping_power_source_name = libam.stoppingPowerSource_no["PSTAR"].value
+    particle_no = libam.AT_particle_no_from_Z_and_A_single(6, 12)
+    material_no = libam.AT_material_number_from_name("Air")
+    print(material_no, particle_no)
+    
+    E_MeV_u = 100.0
+    stopping_power_keV_um = [0.0]
+    stopping_power_keV_um = [0]
+    libam.AT_Stopping_Power(p_stopping_power_source=stopping_power_source_name,
+                            p_E_MeV_u=[E_MeV_u],
+                            p_particle_no=[particle_no],
+                            p_material_no=material_no,
+                            p_stopping_power_keV_um=stopping_power_keV_um)
+
+  
+"""
+
+
 def IonTracks_initial_recombination(voltage_V, energy_MeV, electrode_gap_cm,
                                     PRINT_parameters=False, SHOW_PLOT=False):
     '''
@@ -29,7 +50,7 @@ def IonTracks_initial_recombination(voltage_V, energy_MeV, electrode_gap_cm,
     
     # unit_length_cm = a0_cm
     
-    print(unit_length_cm / a0_cm)
+    # print(unit_length_cm / a0_cm)
 
     LET_keV_um = E_MeV_to_LET_keV_um(energy_MeV)
     track_radius_cm = calc_b_cm(LET_keV_um)
@@ -57,11 +78,11 @@ if __name__ == "__main__":
 
     electrode_gap_cm = 0.1
     voltage_V = 200
-    energy_MeV = 20
-
+    energy_MeV = 20.0
+    
     # LET in air
     LET_keV_um = E_MeV_to_LET_keV_um(energy_MeV)
-
+    
     # calculate the collection effciency with IonTracks and the Jaffe theory
     ks_IonTracks = IonTracks_initial_recombination(voltage_V, energy_MeV, electrode_gap_cm)
     ks_Jaffe = Jaffe_theory(energy_MeV, voltage_V, electrode_gap_cm)
@@ -71,3 +92,8 @@ if __name__ == "__main__":
     header = "# E [MeV], LET [keV/um],  ks_Jaffe, ks_IonTracks\n"
     text = "     {},\t{:0.5f},   {:0.6f},\t{:0.6f}".format(*results)
     print(header, text)
+
+
+
+    
+    
