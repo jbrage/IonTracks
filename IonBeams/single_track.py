22c0d717ab306d2c767a24d3051b2482a7bb5874
jbrage <jeppebrage@gmail.com>
2022-03-18 09:35:23 +0100

    results

--- IonBeams/single_track.py
+++ IonBeams/single_track.py
@@ -72,18 +72,16 @@ def IonTracks_initial_recombination(voltage_V, energy_MeV, electrode_gap_cm, a0_
                                 unit_length_cm
                             ]
 
-    # f_IonTracks = initial_PDEsolver(simulation_parameters)
-    f_IonTracks = Geiss_PDEsolver(simulation_parameters)    
-    return 1./f_IonTracks
+    f_Gauss = initial_PDEsolver(simulation_parameters)
+    f_Geiss = Geiss_PDEsolver(simulation_parameters)    
+    return [1./f_Gauss, 1./f_Geiss]
 
 
-if __name__ == "__main__":
+def calculate_Jaffe_theory(electrode_gap_cm, voltages):
 
-    electrode_gap_cm = 0.2
-    
-    voltages = [50, 150, 200, 225, 250, 275, 400]
-    
-    E_MeV_u_list = range(5, 300, 2)
+    print("calculating Jaffe's correction factors")
+
+    E_MeV_u_list = range(1, 490, 2)
     
     df_J = pd.DataFrame()
     for particle in ["carbon", "neon", "argon", "iron", "proton"]:
@@ -101,38 +99,54 @@ if __name__ == "__main__":
 
                 df_J = df_J.append(row, ignore_index=True)
 	
-    df_J.to_csv("data_Jaffe.csv", index=False)
-    
-        
+    df_J.to_csv("data_Jaffe.csv", index=False)        
     print("\nJaffe finished")	
 
 
 
+if __name__ == "__main__":
+
+    electrode_gap_cm = 0.2
+    
+    voltages = [50, 150, 175, 200, 225, 250, 400]
+    voltages = range(50, 400, 25)
+    
+
+    calculate_Jaffe_theory(electrode_gap_cm, voltages)
+
+    import sys
+    sys.exit()
+    
+
     df = pd.DataFrame()
 
-    for scale in [5, 4, 3, 2.5, 2.0, 1.75]:
-        for voltage_V in voltages:
-             for energy_MeV in range(5, 270, 20):
-                 for use_beta in [False]:
-                     if use_beta:            
-                         a0_nm_list = range(10, 50, 5)
-                     else:
-                         a0_nm_list = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
+    for scale in [3]:
+        for particle in ["carbon", "neon", "argon", "iron", "proton"]:
+            for voltage_V in voltages:
+                for energy_MeV in range(5, 320, 20):
+                    for use_beta in [False]:
+                        if use_beta:            
+                            a0_nm_list = range(10, 50, 5)
+                        else:
+                            a0_nm_list = [0.8, 0.9, 1, 1.25, 1.5]
 
-                     for a0_nm in a0_nm_list:
-                         LET_keV_um = E_MeV_to_LET_keV_um(energy_MeV)
-                         # calculate the collection effciency with IonTracks and the Jaffe theory
+                        for a0_nm in a0_nm_list:
+ 
+                            LET_keV_um = E_MeV_u_to_LET_keV_um(energy_MeV, particle=particle)
+                            water_LET_keV_um = E_MeV_u_to_LET_keV_um(energy_MeV, particle=particle, material="water")
+                            # calculate the collection effciency with IonTracks and the Jaffe theory
 
-                         ks_IonTracks = IonTracks_initial_recombination(voltage_V, energy_MeV, electrode_gap_cm, a0_nm, use_beta, scale)
-                         # ks_Jaffe = Jaffe_theory(energy_MeV, voltage_V, electrode_gap_cm)
+                            ks_Gauss, ks_Geiss = IonTracks_initial_recombination(voltage_V, energy_MeV, electrode_gap_cm, a0_nm, use_beta, scale)
+                            # ks_Jaffe = Jaffe_theory(energy_MeV, voltage_V, electrode_gap_cm)
 
-                         row = {"E_MeV_u": energy_MeV, "LET_keV_um": LET_keV_um, 
-	                        "a0_nm": a0_nm, "ks_IT": ks_IonTracks, "beta": use_beta,
-			        "scale": scale,  "voltage_V": voltage_V, 
-                                }
-                         print(row)
-                         df = df.append(row, ignore_index=True)
+                            row = {"E_MeV_u": energy_MeV, "LET_keV_um": LET_keV_um, "particle": particle,
+	                            "a0_nm": a0_nm, "ks_Gauss": ks_Gauss, "ks_Geiss": ks_Geiss, "beta": use_beta,
+			              "scale": scale,  "voltage_V": voltage_V, "water_LET_keV_um": water_LET_keV_um,
+                               }
+                            print(row)
+                            df = df.append(row, ignore_index=True)
 
-        df.to_csv("result.csv", index=False)
+            df.to_csv("result.csv", index=False)
     
     
+
