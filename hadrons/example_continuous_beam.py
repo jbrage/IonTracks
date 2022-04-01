0495a22d9c4cdfbd4b20b82ca96a74a38bf00895
Jeppe Brage Christensen <jbrage@users.noreply.github.com>
2022-04-01 14:29:33 +0200

    Fixed bug

--- hadrons/example_continuous_beam.py
+++ hadrons/example_continuous_beam.py
@@ -1,19 +1,28 @@
-import numpy as np
 from functions import IonTracks_continuous_beam
 import matplotlib.pyplot as plt
 import pandas as pd
+import itertools
 
-E_MeV_u = 250
-particle = "proton"
-doserate_Gy_min = 100
-voltage_V = 2000
+
+# set parameters
+data_dict = dict(
+    electrode_gap_cm = [0.2],
+    particle = ["proton", "carbon"], 
+    voltage_V = [200],
+    E_MeV_u = [250],
+    doserate_Gy_min = [1, 10, 100, 1000]
+    )
+
+# create a data frame with all the variables
+data_df = pd.DataFrame.from_records(data=itertools.product(*data_dict.values()), 
+                                    columns=data_dict.keys())
 
 IonTracks_df = pd.DataFrame()
-for doserate_Gy_min in [10, 100, 1000]:
-    result_dic = IonTracks_continuous_beam(E_MeV_u=E_MeV_u,
-                                           voltage_V=voltage_V,
-                                           particle=particle,
-                                           doserate_Gy_min=doserate_Gy_min
+for idx, data in data_df.iterrows():
+    result_dic = IonTracks_continuous_beam(E_MeV_u=data.E_MeV_u,
+                                           voltage_V=data.voltage_V,
+                                           particle=data.particle,
+                                           doserate_Gy_min=data.doserate_Gy_min
                                            )
     print(result_dic)
     IonTracks_df = IonTracks_df.append(result_dic, ignore_index=True)
@@ -21,9 +30,7 @@ for doserate_Gy_min in [10, 100, 1000]:
 # plot
 fig, ax = plt.subplots()
 ax.plot(IonTracks_df["doserate_Gy_min"], IonTracks_df["ks_IonTracks"], "ro")
-ax.set_xlim("log")
-
-ax.set_title("{} ions, at {} Gy/mn".format(particle, doserate_Gy_min))
+ax.set_xscale("log")
 ax.set_xlabel("Inverse voltage (1/V)")
 ax.set_ylabel("$k_s$ (IonTracks)")
 
