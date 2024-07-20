d71f250a1ea96ed2633a6d772d6d6a2adfab30fd
retkiewi <krzret@student.agh.edu.pl>
2024-07-20 21:10:40 +0200

    create new python implementations for hadrons

---
+++ hadrons/python/continous_beam.py
@@ -0,0 +1,127 @@
+from dataclasses import dataclass
+from math import exp, sqrt
+from random import Random
+from typing import NDArray, Tuple
+
+import numpy as np
+
+from hadrons.python.generic_hadron_solver import GenericHadronSolver
+
+
+def create_sc_gradients(
+    s: NDArray,
+    c: NDArray,
+) -> Tuple[NDArray, NDArray, float]:
+    sc_pos = np.array(
+        [
+            s[0] + c[0] * (c[0] + 1.0) / 2.0,
+            s[1] + c[1] * (c[1] + 1.0) / 2.0,
+            s[2] + c[2] * (c[2] + 1.0) / 2.0,
+        ]
+    )
+
+    sc_neg = np.array(
+        [
+            s[0] + c[0] * (c[0] - 1.0) / 2.0,
+            s[1] + c[1] * (c[1] - 1.0) / 2.0,
+            s[2] + c[2] * (c[2] - 1.0) / 2.0,
+        ]
+    )
+
+    sc_center = (
+        1.0 - c[0] * c[0] - c[1] * c[1] - c[2] * c[2] - 2.0 * (s[0] + s[1] + s[2])
+    )
+
+    return sc_pos, sc_neg, sc_center
+
+
+@dataclass
+class ContinousHadronSolver(GenericHadronSolver):
+    fluence_rate: float  # [cm-^2/s]
+    # TODO: add a seed param
+    _random_generator: Random = Random(2137)
+
+    @property
+    def buffer_radius(self):
+        return 10
+
+    @property
+    def no_xy(self) -> int:
+        return (
+            int(self.track_radius * self.n_track_radii / self.grid_spacing)
+            + 2 * self.buffer_radius
+        )
+
+    @property
+    def random_generator(self):
+        return self._random_generator
+
+    @property
+    def xy_middle_idx(self):
+        return int(self.no_xy / 2.0)
+
+    @property
+    def inner_radius(self):
+        outer_radius = self.no_xy / 2.0
+
+        return outer_radius - self.buffer_radius
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.simulation_time = self.computation_time_steps * self.dt
+        self.number_of_tracks = int(
+            self.fluence_rate * self.simulation_time * self.track_area
+        )
+        self.number_of_tracks = max(1, self.number_of_tracks)
+
+    def should_insert_track(self, time_step: int) -> bool:
+        return time_step == 0
+
+    def get_random_xy_coordinate(self):
+        return self.random_generator.random() * self.no_xy
+
+    def get_random_coordinates(self):
+        x = self.get_random_xy_coordinate()
+        y = self.get_random_xy_coordinate()
+
+        # check whether the point is inside the circle.
+        # it is too time comsuming simply to set x=rand(0,1)*no_xy
+        while (
+            sqrt((x - self.xy_middle_idx) ** 2 + (y - self.xy_middle_idx) ** 2)
+            > self.inner_radius
+        ):
+            x = self.get_random_xy_coordinate()
+            y = self.get_random_xy_coordinate()
+
+        return x, y
+
+    def get_track_for_time_step(self, time_step: int):
+        positive_array = np.zeros((self.no_xy, self.no_xy, self.no_z_with_buffer))
+        negative_array = np.zeros((self.no_xy, self.no_xy, self.no_z_with_buffer))
+        no_initialised_charge_carriers = 0.0
+
+        x, y = self.get_random_coordinates()
+
+        for k in range(self.no_z_electrode, self.no_z + self.no_z_electrode):
+            for i in range(self.no_xy):
+                for j in range(self.no_xy):
+                    distance_from_center = (
+                        sqrt((i - x) ** 2 + (j - y) ** 2) * self.grid_spacing
+                    )
+                    ion_density = self.Gaussian_factor * exp(
+                        -(distance_from_center**2) / self.track_radius**2
+                    )
+                    positive_array[i, j, k] += ion_density
+                    negative_array[i, j, k] += ion_density
+                    # calculate the recombination only for charge carriers inside the circle
+                    if (
+                        sqrt(
+                            (i - self.xy_middle_idx) ** 2
+                            + (j - self.xy_middle_idx) ** 2
+                        )
+                        < self.inner_radius
+                    ):
+                        if time_step > self.separation_time_steps:
+                            no_initialised_charge_carriers += ion_density
+
+        return positive_array, negative_array, no_initialised_charge_carriers
--- hadrons/python/generic_hadron_solver.py
+++ hadrons/python/generic_hadron_solver.py
@@ -1,11 +1,19 @@
-from __future__ import division
-
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Tuple
+from math import cos, exp, log, pi, sin
+from typing import Literal, NDArray, Tuple
 
 import numpy as np
-from numpy.typing import NDArray
+
+from ..common_properties import (
+    W,
+    air_density_g_cm3,
+    alpha,
+    ion_diff,
+    ion_mobility,
+    water_density_g_cm3,
+)
+from ..geiss_utils import Geiss_r_max, Geiss_RRD_cm
 
 
 def create_sc_gradients(
@@ -36,35 +44,56 @@ def create_sc_gradients(
 
 
 @dataclass
-class GenericElectronSolver(ABC):
-    # Simulation parameters
-    electron_density_per_cm3: float  # fluence-rate [/cm^2/s]
+class GenericHadronSolver(ABC):
+    LET: float  # [keV/um]
     voltage: float  # [V/cm] magnitude of the electric field
+    IC_angle: float  # [rad]
     electrode_gap: float  # [cm]
-    grid_spacing: float
-    ion_mobility: float = 1.73  # cm s^-1 V^-1, averaged for positive and negative ions
-    ion_diff: float = 3.7e-2  # cm^2/s, averaged for positive and negative ions
-    alpha: float = 1.60e-6  # cm^3/s, recombination constant
-    r_cm: float = 0.002  # radius of the sampled cylinder
-    buffer_radius: int = 5
+    E_MeV_u: float
+    a0_nm: float
+    RDD_model: str
+    grid_spacing: float  # [cm]
+    track_radius: float  # [cm]
+    RDD_model: Literal["Gauss", "Geiss"]
     no_z_electrode: int = (
-        5  # length of the electrode-buffer to ensure no ions drift through the array in one time step
+        4  # length of the electrode-buffer to ensure no ions drift through the array in one time step
     )
-    dt: float = 1.0
+    n_track_radii: int = 6  # scaling factor to determine the grid width
 
-    # Derived properties
+    @property
+    def normalized_LET(self) -> float:
+        return self.LET * 1e7
+
+    @property
+    def a0(self) -> float:
+        density_ratio = water_density_g_cm3 / air_density_g_cm3
+        return self.a0_nm * 1e-7 * density_ratio
+
+    @property
+    def r_max(self) -> float:
+        return Geiss_r_max(self.E_MeV_u, air_density_g_cm3)
+
+    @property
+    def c(self) -> float:
+        return (
+            self.normalized_LET / (pi * W) * (1 / (1 + 2 * log(self.r_max / self.a0)))
+        )
+
+    @property
+    def track_area(self) -> float:
+        return pi * self.track_radius**2
+
+    @property
+    def Gaussian_factor(self) -> float:
+        N0 = self.normalized_LET / W
+        return N0 / self.track_area
+
     @property
     def no_xy(self) -> int:
-        """Number of voxels om the xy-directions"""
-
-        no_xy = int(2 * self.r_cm / self.grid_spacing)
-        no_xy += 2 * self.buffer_radius
-        return no_xy
+        return int(self.track_radius * self.n_track_radii / self.grid_spacing)
 
     @property
     def no_z(self) -> int:
-        """Number of voxels om the z-direction"""
-
         return int(self.electrode_gap / self.grid_spacing)
 
     @property
@@ -78,15 +107,23 @@ class GenericElectronSolver(ABC):
     @property
     def computation_time_steps(self) -> int:
         separation_time_steps = int(
-            self.electrode_gap
-            / (2.0 * self.ion_mobility * self.electric_field * self.dt)
+            self.electrode_gap / (2.0 * ion_mobility * self.electric_field * self.dt)
         )
 
-        return separation_time_steps * 3
+        return separation_time_steps * 2
 
     @property
-    def delta_border(self) -> int:
-        return 2
+    def RDD_function(self):
+        if self.RDD_model == "Gauss":
+            return lambda r_cm: self.Gaussian_factor * exp(
+                -(r_cm**2) / self.track_radius**2
+            )
+        elif self.RDD_model == "Geiss":
+            return lambda r_cm: Geiss_RRD_cm(r_cm, self.c, self.a0, self.r_max)
+        else:
+            raise ValueError(
+                f"Invalid RDD model: {self.RDD_model}. Must be 'Gauss' or 'Geiss'."
+            )
 
     def von_neumann_expression(self) -> Tuple[float, NDArray, NDArray]:
         """
@@ -97,20 +134,28 @@ class GenericElectronSolver(ABC):
         von_neumann_expression = False
         dt = 1.0
 
-        # initialize the coefficients
-        sx = sy = sz = cx = cy = cz = 0.0
-
         while not von_neumann_expression:
             dt /= 1.01
             # as defined in the Deghan (2004) paper
+            sx = ion_diff * dt / (self.grid_spacing**2)
+            sy = sz = sx
 
-            # we leave the coeficents as 0 in the xy plane
-            # sx = ion_diff*dt/(self.grid_spacing_cm**2)
-            # sy = ion_diff*dt/(self.grid_spacing_cm**2)
+            cx = (
+                ion_mobility
+                * self.electric_field
+                * dt
+                / self.grid_spacing
+                * sin(self.IC_angle)
+            )
+            cy = 0
+            cz = (
+                ion_mobility
+                * self.electric_field
+                * dt
+                / self.grid_spacing
+                * cos(self.IC_angle)
+            )
 
-            sz = self.ion_diff * dt / (self.grid_spacing**2)
-            cz = self.ion_mobility * self.electric_field * dt / self.grid_spacing
-            # check von Neumann's criterion
             criterion_1 = 2 * (sx + sy + sz) + cx**2 + cy**2 + cz**2 <= 1
 
             criterion_2 = cx**2 * cy**2 * cz**2 <= 8 * sx * sy * sz
@@ -119,9 +164,7 @@ class GenericElectronSolver(ABC):
 
         return dt, np.array([sx, sy, sz]), np.array([cx, cy, cz])
 
-    def __post_init__(
-        self,
-    ):
+    def __post_init__(self):
         # depending on the cluster/computer, the upper limit may be changed
         if (self.no_xy * self.no_xy * self.no_z) > 1e8:
             raise ValueError(
@@ -132,65 +175,42 @@ class GenericElectronSolver(ABC):
         self.dt, self.s, self.c = self.von_neumann_expression()
 
     @abstractmethod
-    def should_simulate_beam_for_time_step(self, time_step: int) -> bool:
+    def should_insert_track(self, time_step: int) -> bool:
         pass
 
     @abstractmethod
-    def update_electron_density_arrays_after_beam(
-        self, positive_array: NDArray, negative_array: NDArray
-    ) -> None:
+    def get_track_for_time_step(
+        self, time_step: int
+    ) -> Tuple[NDArray[np.float64], NDArray[np.float64], float]:
         pass
-
-    @abstractmethod
-    def get_initialised_charge_carriers_after_beam() -> float:
-        pass
-
-    def simulate_beam(self, positive_array: NDArray, negative_array: NDArray):
-        self.update_electron_density_arrays_after_beam(positive_array, negative_array)
-
-        return self.get_initialised_charge_carriers_after_beam()
 
     def calculate(self):
         positive_array = np.zeros((self.no_xy, self.no_xy, self.no_z_with_buffer))
         negative_array = np.zeros((self.no_xy, self.no_xy, self.no_z_with_buffer))
-        positive_array_temp = np.zeros((self.no_xy, self.no_xy, self.no_z_with_buffer))
-        negative_array_temp = np.zeros((self.no_xy, self.no_xy, self.no_z_with_buffer))
-        no_recombined_charge_carriers = 0.0
         no_initialised_charge_carriers = 0.0
 
-        """
-        Create an array which includes the number of track to be inserted for each time step
-        The tracks are distributed uniformly in time
-        """
+        positive_array_temp = np.zeros((self.no_xy, self.no_xy, self.no_z_with_buffer))
+        negative_array_temp = np.zeros((self.no_xy, self.no_xy, self.no_z_with_buffer))
 
-        positive_temp_entry = negative_temp_entry = recomb_temp = 0.0
-
-        f_steps_list = np.zeros(self.computation_time_steps)
+        no_recombined_charge_carriers = 0.0
 
         sc_pos, sc_neg, sc_center = create_sc_gradients(self.s, self.c)
 
-        """
-        Start the simulation by evolving the distribution one step at a time
-        """
-
         for time_step in range(self.computation_time_steps):
-
-            """
-            Refill the array with the electron density each time step
-            """
-            if self.should_simulate_beam_for_time_step(time_step):
-                no_initialised_step = self.simulate_beam(
-                    positive_array,
-                    negative_array,
-                )
-            else:
-                no_initialised_step = 0.0
-
-            no_initialised_charge_carriers += no_initialised_step
-
             # calculate the new densities and store them in temporary arrays
-            for i in range(1, self.no_xy - 1):
-                for j in range(1, self.no_xy - 1):
+
+            if self.should_insert_track(time_step):
+                positive_track, negative_track, initialized_carriers = (
+                    self.get_track_for_time_step(time_step)
+                )
+
+                positive_array += positive_track
+                negative_array += negative_track
+
+                no_initialised_charge_carriers += initialized_carriers
+
+            for i in range(1, self.no_x - 1):
+                for j in range(1, self.no_x - 1):
                     for k in range(1, self.no_z_with_buffer - 1):
                         # using the Lax-Wendroff scheme
                         positive_temp_entry = 0
@@ -222,24 +242,28 @@ class GenericElectronSolver(ABC):
 
                         # the recombination part
                         recomb_temp = (
-                            self.alpha
+                            alpha
                             * positive_array[i, j, k]
                             * negative_array[i, j, k]
                             * self.dt
                         )
+
                         positive_array_temp[i, j, k] = positive_temp_entry - recomb_temp
                         negative_array_temp[i, j, k] = negative_temp_entry - recomb_temp
 
-                        if k > self.no_z_electrode and k < (
-                            self.no_z + self.no_z_electrode
-                        ):
-                            # sum over the recombination between the virtual electrodes
-                            no_recombined_charge_carriers += recomb_temp
+                        no_recombined_charge_carriers += recomb_temp
 
-            f_steps_list[time_step] = (
-                no_initialised_charge_carriers - no_recombined_charge_carriers
-            ) / no_initialised_charge_carriers
-            positive_array[:] = positive_array_temp
-            negative_array[:] = negative_array_temp
+            # update the positive and negative arrays
 
-        return f_steps_list
+            # TODO: check if copying the first element in each dimension is necessary
+            # positive_array[1:-1, 1:-1, 1:-1] = positive_array_temp[1:-1, 1:-1, 1:-1]
+            # negative_array[1:-1, 1:-1, 1:-1] = negative_array_temp[1:-1, 1:-1, 1:-1]
+
+            positive_array[:] = positive_array_temp[:]
+            negative_array[:] = negative_array_temp[:]
+
+        f = (
+            no_initialised_charge_carriers - no_recombined_charge_carriers
+        ) / no_initialised_charge_carriers
+
+        return 1.0 / f
